f1b7f9b8fedf9a7a6a60596351a97aedfe3a414a
WYDERKAAA <maciekwydra@yahoo.com>
2024-06-17 19:22:07 +0200

    11

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,17 @@
-const categoriesList = document.getElementById("categories");
-const categories = categoriesList.querySelectorAll(".item");
-let categoryCount = 0;
-categories.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const items = category.querySelectorAll("ul > li");
-  let itemCount = items.length;
-  console.log(`Kategoria: ${categoryName}, Liczba elementów: ${itemCount}`);
-  categoryCount++;
-});
-console.log(`Liczba kategorii: ${categoryCount}`);
+const images = [
+  {
+    preview:
+      "<https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg>",
+    original:
+      "<https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg>",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "<https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg>",
+    original:
+      "<https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg>",
+    description: "Container Haulage Freight",
+  },
+  // Pozostałe obiekty obrazów...
+];
